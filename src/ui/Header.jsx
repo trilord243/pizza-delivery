916306528a3c5818aab2cd22dee64dae3312e406
Felipe Escalona <escalonaf12@gmail.com>
2024-01-02 00:59:02 -0400

    routes and components

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <header>
+
+            <Link to="/">Trilords Pizza co.</Link>
+
+            <p>Felipe</p>
+        </header>
+    )
+}
